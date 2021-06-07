4cd98e303857e77dfa462d8e1ca9f818c2d5d3e6
梅涛 <573945966@qq.com>
2021-06-07 15:04:55 +0800

    CONSUL-CLIENT

---
+++ 03_consul-client/src/main/java/com/ink/ConsulClientApplication.java
@@ -0,0 +1,17 @@
+package com.ink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author MT
+ * @date 2021/6/7 14:58
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsulClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulClientApplication.class, args);
+    }
+}
