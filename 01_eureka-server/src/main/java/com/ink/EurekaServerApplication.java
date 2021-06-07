dba4c5accee454d7d18554eae9e36c8ac2384523
梅涛 <573945966@qq.com>
2021-06-07 14:21:45 +0800

    EUREKA-SERVER

---
+++ 01_eureka-server/src/main/java/com/ink/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.ink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author MT
+ * @date 2021/6/7 14:04
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
