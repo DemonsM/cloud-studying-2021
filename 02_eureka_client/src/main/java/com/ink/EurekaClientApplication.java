1949d83da1fb8189040b8a7b1d83b479a14aeee0
梅涛 <573945966@qq.com>
2021-06-07 14:51:46 +0800

    EUREKA-CLIENT

---
+++ 02_eureka_client/src/main/java/com/ink/EurekaClientApplication.java
@@ -0,0 +1,17 @@
+package com.ink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author MT
+ * @date 2021/6/7 14:35
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
